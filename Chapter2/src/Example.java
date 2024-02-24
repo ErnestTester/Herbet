ec75fc5a37c4822034a3facb01704ed0cd8a9e93
Ernesto Alonso <156559888+ErnestTester@users.noreply.github.com>
2024-02-23 19:29:46 -0600

    Add a new Class

---
+++ Chapter2/src/Example.java
@@ -0,0 +1,11 @@
+/*This is a simple Java program
+call this file Example.java*/
+
+
+public class Example
+{
+    public static void main(String[] args) {
+        System.out.println("This is a Simple Java program!");
+
+    }
+}
