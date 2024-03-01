8e856eabc98ac7d88f858ce7e97ced17a27791a5
Ernesto Alonso <156559888+ErnestTester@users.noreply.github.com>
2024-02-29 21:37:10 -0600

    Add a new Class

---
+++ Chapter2/src/Example2.java
@@ -0,0 +1,15 @@
+public class Example2 {
+    public static void main(String[] args) {
+        int num; //This declares a variable name num
+
+        num=100; //This assign the num value 100
+
+        System.out.println("This is num: "+num);
+
+        num=num*2;
+
+        System.out.print("The value of num  is * 2 is: ");
+        System.out.println(num);
+
+    }
+}
