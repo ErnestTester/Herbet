c09f43e3f609624bd9a700dc779549d928ca182b
Ernesto Alonso <156559888+ErnestTester@users.noreply.github.com>
2024-03-02 18:25:28 -0600

    Add new class

---
+++ Chapter3/src/Light.java
@@ -0,0 +1,15 @@
+public class Light
+{
+    public static void main(String[] args) {
+
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+
+        //approximate speed of the light in miles per second
+        lightspeed=186000;
+
+
+    }
+}
