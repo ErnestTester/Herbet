d24802004ae2c5bfac6249974e9978501a9fbb91
Ernesto Alonso <156559888+ErnestTester@users.noreply.github.com>
2024-03-02 18:48:52 -0600

    New lines

--- Chapter3/src/Light.java
+++ Chapter3/src/Light.java
@@ -1,3 +1,5 @@
+import com.sun.security.jgss.GSSUtil;
+
 public class Light
 {
     public static void main(String[] args) {
@@ -9,6 +11,16 @@ public class Light
 
         //approximate speed of the light in miles per second
         lightspeed=186000;
+
+        days=1;  // specify the numbers of days here
+
+        seconds=days*24*60*60;
+
+        distance=lightspeed * seconds;
+
+        System.out.println("In days "+days);
+        System.out.println("days light will travel about");
+        System.out.println(distance + " miles");
 
 
     }
