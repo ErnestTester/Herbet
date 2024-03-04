068c5a78cbbf5b105e5f115b15f0823182510fac
Ernesto Alonso <156559888+ErnestTester@users.noreply.github.com>
2024-03-04 15:36:31 -0600

    update lines

--- Chapter3/src/CharDemo.java
+++ Chapter3/src/CharDemo.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Char {
+public class CharDemo {
+
+    public static void main(String[] args) {
+        char ch1, ch2;
+        ch1=88; // code for X
+        ch2='Y';
+
+        System.out.println("ch1 and ch2: ");
+        System.out.println(ch1 + " "+ch2);
+
+
+    }
 }
