876e6616d2d5ca9a2eff0625e1e3fc811114c00c
Ernesto Alonso <156559888+ErnestTester@users.noreply.github.com>
2024-03-04 07:42:53 -0600

    New lines

--- Chapter3/src/Area.java
+++ Chapter3/src/Area.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Area {
+public class Area {
+
+    public static void main(String[] args) {
+        double pi,r,a;
+
+        r=10.8;
+        pi=3.1416;
+
+        a=pi * r*r;
+        System.out.println("Area of a circle is: "+a);
+    }
 }
