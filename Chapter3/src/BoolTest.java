2d7450ac2216f3bf4c183a5ce0960dc2936f9a46
Ernesto Alonso <156559888+ErnestTester@users.noreply.github.com>
2024-03-04 16:30:57 -0600

    Update code

--- Chapter3/src/BoolTest.java
+++ Chapter3/src/BoolTest.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class BoolTest {
+public class BoolTest {
+    public static void main(String[] args) {
+
+        boolean b;
+
+        b=false;
+        System.out.println("b is :"+b);
+        b=true;
+        System.out.println("b is: "+b);
+
+        if(b) System.out.println("This is executed");
+        b=false;
+        if(b) System.out.println("This is not executed.!");
+    }
 }
